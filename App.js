b358b67698825fa06b281c644047f911e9a7b80b
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2018-03-06 12:15:09 +0900

    [add] できた

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View, AlertIOS} from 'react-native';
 
 export default class App extends React.Component {
   render() {
-    return (
+		return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+				<Button
+					title="購入する"
+					onPress={() => {
+						// TouchIDまたは顔認証が利用可能か
+						Expo.Fingerprint.hasHardwareAsync()
+							.then(isAvailable => {
+								if (isAvailable) {
+									// TouchIDまたは顔認証起動
+									Expo.Fingerprint.authenticateAsync()
+										.then(result => {
+											console.log(result);
+											if (result.success) {
+												setTimeout(() => {
+													AlertIOS.alert(
+														'購入が完了しました',
+													);
+												}, 1000)
+											}
+										});
+								}
+							})
+					}}
+				/>
       </View>
     );
   }
